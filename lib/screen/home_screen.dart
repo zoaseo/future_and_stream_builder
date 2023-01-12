f2e0cf2f54ef6523582c992c61296d72474d8d9c
zoaseo <101240239+zoaseo@users.noreply.github.com>
2023-01-12 11:14:21 +0900

    upload

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,79 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      fontSize: 16.0,
+    );
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: StreamBuilder<int>(
+          stream: streamNumbers(),
+          builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'FutureBuilder',
+                  style: textStyle.copyWith(
+                      fontWeight: FontWeight.w700, fontSize: 20.0),
+                ),
+                Text(
+                  'ConState : ${snapshot.connectionState}',
+                  style: textStyle,
+                ),
+                Text(
+                  'Data : ${snapshot.data}',
+                  style: textStyle,
+                ),
+                Text(
+                  'Error : ${snapshot.error}',
+                  style: textStyle,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  child: Text(
+                    'SetState',
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<int> getNumber() async {
+    await Future.delayed(Duration(seconds: 3));
+    final random = Random();
+
+    return random.nextInt(100);
+  }
+
+  Stream<int> streamNumbers() async* {
+    for (int i = 0; i < 10; i++) {
+      if (i == 5) {
+        throw Exception('i = 5');
+      }
+
+      await Future.delayed(Duration(seconds: 1));
+
+      yield i;
+    }
+  }
+}
